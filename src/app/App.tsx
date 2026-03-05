18549cf9a6b3ae8c81c31c8a649b5763a05f670c
Sean Botha <seanbotha@outlook.com>
2026-03-05 14:36:06 +0200

    Add production React folder structure

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import Navbar from "./components/Navbar"
+import Navbar from "../components/layout/Navbar"
 
 function App() {
   const [taskCount, setTaskCount] = useState(0)
